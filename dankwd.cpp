3c8fecf1fe9e86015be14fc281d46b044bfa149c
Ryan Dencker <ryandencker>
2024-02-07 00:46:27 -0800

    added password complexity

--- dankwd.cpp
+++ dankwd.cpp
@@ -4,8 +4,11 @@
 #include <unordered_set>
 #include <random>
 #include <windows.h>
+#include <cctype>
 
 using namespace std;
+
+char specialchars[] = "!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
 
 unordered_set<string> loadPasswords(const string& filename) {
     unordered_set<string> passwords;
@@ -32,7 +35,7 @@ void help()
     cout << "Options:" << endl;
     cout << "\t -t \t compares password to large wordlists of common passwords" << endl;
     cout << "\t -p \t checks strength of password" << endl;
-    cout << "\t -d \t checks how long it will take to crack password" << endl;
+    cout << "\t -d \t checks how long it will take to crack password assuming 1 million guesses per second" << endl;
     cout << "\t -a \t does all the flags" << endl;
     exit(0);
 }
@@ -99,16 +102,87 @@ void test(string usrpswd)
     return;
 }
 
-void password()
+void password(string usrpswd)
 {
     //check password for uppercase, lowercase, special characters
     cout << "-p" << endl;
 }
 
-void crack()
+bool hasNums(string usrpswd)
+{
+    for (char c : usrpswd) {
+        if (isdigit(c)) {
+            return true; // If a digit is found, return true immediately
+        }
+    }
+    return false;
+}
+
+bool haslLetters(string usrpswd)
+{
+    for (char c : usrpswd) {
+        if (islower(c)) {
+            return true; // If a lowercase letter is found, set hasLower to true
+        }
+    }
+    return false;
+}
+
+bool hasuLetters(string usrpswd)
+{
+    for (char c : usrpswd) {
+        if (isupper(c)) {
+            return true; // If a uppercase letter is found, set hasLower to true
+        }
+    }
+    return false;
+}
+
+bool hasSpecials(string usrpswd)
+{
+    for (char c : usrpswd) {
+        for (int i = 0; i < strlen(specialchars); ++i) {
+            if (c == specialchars[i]) {
+                return true; // If the character matches any special character, return true
+            }
+        }
+    }
+    return false;
+}
+
+void crack(string usrpswd)
 {
     //calculate how long it will take to crack assuming it is a strong password
-    cout << "-c" << endl;
+    //combinations - numbers 10 each char   -   lower letters 26 each char   -   upper letters 26 each char   -   special 32 each char
+    //for total combination, we will 
+    bool nums = false;
+    bool lLetters = false;
+    bool uLetters = false;
+    bool specials = false; 
+
+    if(hasNums(usrpswd))
+    {
+        nums = true;
+        cout << "password has nums";
+    }
+
+    if(haslLetters(usrpswd))
+    {
+        lLetters = true;
+        cout << "password has lower letters";
+    }
+
+    if(hasuLetters(usrpswd))
+    {
+        uLetters = true;
+        cout << "password has upper letters";
+    }
+
+    if(hasSpecials(usrpswd))
+    {
+        specials = true;
+        cout << "password has special characters";
+    }
 }
 
 void all()
@@ -148,9 +222,9 @@ int main(int argc, char *argv[])    //argc is the number of commandline argument
     if (flag == "-t")
         test(usrpswd);
     else if (flag == "-p")
-        password();
+        password(usrpswd);
     else if (flag == "-d")
-        crack();
+        crack(usrpswd);
     else if (flag == "-a")
         all();
     else
