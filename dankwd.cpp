3d9efe7a0cfd9016c6ac7b9e0c1bdfd8dda35fff
Ryan Dencker <ryanddencker@gmail.com>
2024-02-21 02:19:28 -0800

    added danktext and worked on crack()

--- dankwd.cpp
+++ dankwd.cpp
@@ -5,6 +5,8 @@
 #include <random>
 #include <windows.h>
 #include <cctype>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -31,11 +33,11 @@ unordered_set<string> loadPasswords(const string& filename) {
 void help()
 {
     cout << "\n \nVersion 1.0" << endl;
-    cout << "Usage:     dankwd <flag> <password>" << endl;
+    cout << "Usage:     dankwd.exe <flag> <password>" << endl;
     cout << "Options:" << endl;
     cout << "\t -t \t compares password to large wordlists of common passwords" << endl;
     cout << "\t -p \t checks strength of password" << endl;
-    cout << "\t -d \t checks how long it will take to crack password assuming 1 million guesses per second" << endl;
+    cout << "\t -c \t checks how long it will take to crack password assuming 1 billion guesses per second" << endl;
     cout << "\t -a \t does all the flags" << endl;
     exit(0);
 }
@@ -224,29 +226,26 @@ void crack(string usrpswd)
     bool specials = false; 
     int combinations;
     int totalCharacters = 0;
+    string response;
 
     if(hasNums(usrpswd))
     {
         nums = true;
-        cout << "password has nums";
     }
 
     if(haslLetters(usrpswd))
     {
         lLetters = true;
-        cout << "password has lower letters";
     }
 
     if(hasuLetters(usrpswd))
     {
         uLetters = true;
-        cout << "password has upper letters";
     }
 
     if(hasSpecials(usrpswd))
     {
         specials = true;
-        cout << "password has special characters";
     }
 
     if (nums) totalCharacters += 10;        // 10 numbers (0-9)
@@ -256,7 +255,46 @@ void crack(string usrpswd)
 
     combinations = pow(totalCharacters, usrpswd.length());
 
-    cout << "Total Combinations: " << combinations << endl;
+    cout << "\nTotal Combinations: " << combinations << endl;
+
+    
+
+    while(true)
+    {
+        cout << endl << "How many attempts per second would you like to attempt to crack the password?\n\n";
+
+        cout << "a: 1 million attempts per second\n";
+        cout << "b: 10 million attempts per second\n";
+        cout << "c: 100 million attempts per second\n";
+        cout << "d: 1 billion attempts per second\n";
+        cout << "e: enter custom attempts per second\n";
+        cin >> response;
+
+        if(response == "a")
+        {
+
+        }
+        else if(response == "b")
+        {
+
+        }
+        else if(response == "c")
+        {
+
+        }
+        else if(response == "d")
+        {
+
+        }
+        else if(response == "e")
+        {
+
+        }
+        else
+        {
+            cout << "choose from the selected options"; //finish this
+        }
+    }
 
 }
 
@@ -266,14 +304,180 @@ void all()
     cout << "-a" << endl;
 }
 
+void danktext()
+{
+    srand((unsigned) time(NULL));
+    int random = (rand() % 9);
+    
+    switch(random)
+    {
+        case 0:
+        {
+            cout << R"(
+    ,---,                                  ,-.                           
+  .'  .' `\                            ,--/ /|                     ,---, 
+,---.'     \                   ,---, ,--. :/ |          .---.    ,---.'| 
+|   |  .`\  |              ,-+-. /  |:  : ' /          /. ./|    |   | : 
+:   : |  '  |  ,--.--.    ,--.'|'   ||  '  /        .-'-. ' |    |   | | 
+|   ' '  ;  : /       \  |   |  ,"' |'  |  :       /___/ \: |  ,--.__| | 
+'   | ;  .  |.--.  .-. | |   | /  | ||  |   \   .-'.. '   ' . /   ,'   | 
+|   | :  |  ' \__\/: . . |   | |  | |'  : |. \ /___/ \:     '.   '  /  | 
+'   : | /  ;  ," .--.; | |   | |  |/ |  | ' \ \.   \  ' .\   '   ; |:  | 
+|   | '` ,/  /  /  ,.  | |   | |--'  '  : |--'  \   \   ' \ ||   | '/  ' 
+;   :  .'   ;  :   .'   \|   |/      ;  |,'      \   \  |--" |   :    :| 
+|   ,.'     |  ,     .-./'---'       '--'         \   \ |     \   \  /   
+'---'        `--`---'                              '---"       `----'    
+                                                                        
+            )";
+            break;
+        }
+        
+        case 1:
+        {
+            cout << R"(
+ ______        ____    ,---.   .--..--.   .--.  .--.      .--. ______      
+|    _ `''.  .'  __ `. |    \  |  ||  | _/  /   |  |_     |  ||    _ `''.  
+| _ | ) _  \/   '  \  \|  ,  \ |  || (`' ) /    | _( )_   |  || _ | ) _  \ 
+|( ''_'  ) ||___|  /  ||  |\_ \|  ||(_ ()_)     |(_ o _)  |  ||( ''_'  ) | 
+| . (_) `. |   _.-`   ||  _( )_\  || (_,_)   __ | (_,_) \ |  || . (_) `. | 
+|(_    ._) '.'   _    || (_ o _)  ||  |\ \  |  ||  |/    \|  ||(_    ._) ' 
+|  (_.\.' / |  _( )_  ||  (_,_)\  ||  | \ `'   /|  '  /\  `  ||  (_.\.' /  
+|       .'  \ (_ o _) /|  |    |  ||  |  \    / |    /  \    ||       .'   
+'-----'`     '.(_,_).' '--'    '--'`--'   `'-'  `---'    `---`'-----'`    
+            )";
+            break;
+        }
+
+        case 2:
+        {
+            cout << R"(
+      :::::::::      :::     ::::    ::: :::    ::: :::       ::: ::::::::: 
+     :+:    :+:   :+: :+:   :+:+:   :+: :+:   :+:  :+:       :+: :+:    :+: 
+    +:+    +:+  +:+   +:+  :+:+:+  +:+ +:+  +:+   +:+       +:+ +:+    +:+  
+   +#+    +:+ +#++:++#++: +#+ +:+ +#+ +#++:++    +#+  +:+  +#+ +#+    +:+   
+  +#+    +#+ +#+     +#+ +#+  +#+#+# +#+  +#+   +#+ +#+#+ +#+ +#+    +#+    
+ #+#    #+# #+#     #+# #+#   #+#+# #+#   #+#   #+#+# #+#+#  #+#    #+#     
+#########  ###     ### ###    #### ###    ###   ###   ###   #########       
+            )";
+            break;
+        }
+
+        case 3:
+        {
+            cout << R"(
+__/\\\\\\\\\\\\_________________________________________________________________________/\\\__        
+ _\/\\\////////\\\________________________________/\\\__________________________________\/\\\__       
+  _\/\\\______\//\\\______________________________\/\\\__________________________________\/\\\__      
+   _\/\\\_______\/\\\__/\\\\\\\\\_____/\\/\\\\\\___\/\\\\\\\\_____/\\____/\\___/\\________\/\\\__     
+    _\/\\\_______\/\\\_\////////\\\___\/\\\////\\\__\/\\\////\\\__\/\\\__/\\\\_/\\\___/\\\\\\\\\__    
+     _\/\\\_______\/\\\___/\\\\\\\\\\__\/\\\__\//\\\_\/\\\\\\\\/___\//\\\/\\\\\/\\\___/\\\////\\\__   
+      _\/\\\_______/\\\___/\\\/////\\\__\/\\\___\/\\\_\/\\\///\\\____\//\\\\\/\\\\\___\/\\\__\/\\\__  
+       _\/\\\\\\\\\\\\/___\//\\\\\\\\/\\_\/\\\___\/\\\_\/\\\_\///\\\___\//\\\\//\\\____\//\\\\\\\/\\_ 
+        _\////////////______\////////\//__\///____\///__\///____\///_____\///__\///______\///////\//__
+            )";
+            break;
+        }
+
+        case 4:
+        {
+            cout << R"(
+ /$$$$$$$                      /$$                           /$$
+| $$__  $$                    | $$                          | $$
+| $$  \ $$  /$$$$$$  /$$$$$$$ | $$   /$$ /$$  /$$  /$$  /$$$$$$$
+| $$  | $$ |____  $$| $$__  $$| $$  /$$/| $$ | $$ | $$ /$$__  $$
+| $$  | $$  /$$$$$$$| $$  \ $$| $$$$$$/ | $$ | $$ | $$| $$  | $$
+| $$  | $$ /$$__  $$| $$  | $$| $$_  $$ | $$ | $$ | $$| $$  | $$
+| $$$$$$$/|  $$$$$$$| $$  | $$| $$ \  $$|  $$$$$/$$$$/|  $$$$$$$
+|_______/  \_______/|__/  |__/|__/  \__/ \_____/\___/  \_______/
+            )";
+            break;
+        }
+        
+        case 5:
+        {
+            cout << R"(
+    ____              __                __
+   / __ \____ _____  / /___      ______/ /
+  / / / / __ `/ __ \/ //_/ | /| / / __  / 
+ / /_/ / /_/ / / / / ,<  | |/ |/ / /_/ /  
+/_____/\__,_/_/ /_/_/|_| |__/|__/\__,_/  
+            )";
+            break;
+        }
+
+        case 6:
+        {
+            cout << R"(
+ (                                        
+ )\ )                   )           (     
+(()/(      )         ( /(  (  (     )\ )  
+ /(_))  ( /(   (     )\()) )\))(   (()/(  
+(_))_   )(_))  )\ ) ((_)\ ((_)()\   ((_)) 
+ |   \ ((_)_  _(_/( | |(_)_(()((_)  _| |  
+ | |) |/ _` || ' \))| / / \ V  V // _` |  
+ |___/ \__,_||_||_| |_\_\  \_/\_/ \__,_| 
+            )";
+            break;
+        }
+
+        case 7:
+        {
+            cout << R"(
+  ;                                                                  ;           
+  ED.                                                                ED.         
+  E#Wi                     L.             G:                         E#Wi        
+  E###G.                   EW:        ,ft E#,    :                   E###G.      
+  E#fD#W;               .. E##;       t#E E#t  .GE          ;        E#fD#W;     
+  E#t t##L             ;W, E###t      t#E E#t j#K;        .DL        E#t t##L    
+  E#t  .E#K,          j##, E#fE#f     t#E E#GK#f  f.     :K#L     LWLE#t  .E#K,  
+  E#t    j##f        G###, E#t D#G    t#E E##D.   EW:   ;W##L   .E#f E#t    j##f 
+  E#t    :E#K:     :E####, E#t  f#E.  t#E E##Wi   E#t  t#KE#L  ,W#;  E#t    :E#K:
+  E#t   t##L      ;W#DG##, E#t   t#K: t#E E#jL#D: E#t f#D.L#L t#K:   E#t   t##L  
+  E#t .D#W;      j###DW##, E#t    ;#W,t#E E#t ,K#jE#jG#f  L#LL#G     E#t .D#W;   
+  E#tiW#G.      G##i,,G##, E#t     :K#D#E E#t   jDE###;   L###j      E#tiW#G.    
+  E#K##i      :K#K:   L##, E#t      .E##E j#t     E#K:    L#W;       E#K##i      
+  E##D.      ;##D.    L##, ..         G#E  ,;     EG      LE.        E##D.       
+  E#t        ,,,      .,,              fE         ;       ;@         E#t         
+  L:                                    ,                            L:         
+            )";
+            break;
+        }
+
+        case 8:
+        {
+            cout << R"(
+ ____ ____ ____ ____ ____ ____ 
+||D |||a |||n |||k |||w |||d ||
+||__|||__|||__|||__|||__|||__||
+|/__\|/__\|/__\|/__\|/__\|/__\|
+            )";
+            break;
+        }
+
+        case 9:
+        {
+            cout << R"(
+________              ______           _________
+___  __ \_____ __________  /____      _______  /
+__  / / /  __ `/_  __ \_  //_/_ | /| / /  __  / 
+_  /_/ // /_/ /_  / / /  ,<  __ |/ |/ // /_/ /  
+/_____/ \__,_/ /_/ /_//_/|_| ____/|__/ \__,_/   
+            )";
+            break;
+        }
+    }
+
+
+}
+
 int main(int argc, char *argv[])    //argc is the number of commandline arguments recieved, argv is the strings
 {
     
 
-    //random dankwd text
+    danktext();
 
 
-    if(argc == 1 || argc == 2)   //if user only runs the executable
+    if(argc == 1 || argc == 2)   //if user only runs the executable or doesnt provide a password
     {
         help();
     }
@@ -294,7 +498,7 @@ int main(int argc, char *argv[])    //argc is the number of commandline argument
     else if (flag == "-p")
         password(usrpswd);  //in progress
     else if (flag == "-c")
-        crack(usrpswd);     //in progress
+        crack(usrpswd);     //in progress first
     else if (flag == "-a")
         all();
     else
